bc8cae7067f9226065fd3145879ebb00674de847
Mathieu Rouillard <45506734+rouillardm@users.noreply.github.com>
2021-11-30 01:00:02 -0500

    OCR with cam

--- src/components/CameraView.tsx
+++ src/components/CameraView.tsx
@@ -1,24 +1,26 @@
-import React, { useRef } from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useRef } from 'react';
+import { StyleSheet, useWindowDimensions, View } from 'react-native';
 
-import { Camera, useCameraDevices } from "react-native-vision-camera";
+import { Camera, useCameraDevices } from 'react-native-vision-camera';
 import Reanimated from 'react-native-reanimated';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { IconButton } from "react-native-paper";
+import { IconButton } from 'react-native-paper';
+
+import MlkitOcr from 'react-native-mlkit-ocr';
+import RNPhotoManipulator from 'react-native-photo-manipulator';
 
 const ReanimatedCamera = Reanimated.createAnimatedComponent(Camera);
 Reanimated.addWhitelistedNativeProps({
   zoom: true,
 });
 
-const CameraView = (props: {
-  isActive: boolean,
-}) => {
+const CameraView = (props: { isActive: boolean }) => {
   const camera = useRef<Camera>(null);
   const devices = useCameraDevices();
   const device = devices.back;
   const insets = useSafeAreaInsets();
 
+  const { height: winH, width: _winW } = useWindowDimensions();
   return (
     <View style={styles.container}>
       {device != null && (
@@ -32,25 +34,42 @@ const CameraView = (props: {
         />
       )}
 
-      <View
-        style={{
-          position: 'absolute',
-          alignSelf: 'center',
-          width: '75%',
-          height: 175,
-          borderWidth: 2,
-          borderColor: 'rgba(255, 255, 255, 0.75)',
-        }}
-      />
+      <View style={styles.cameraOverlay} />
 
       <IconButton
-        icon="camera"
-        color="white"
+        icon='camera'
+        color='white'
         size={40}
         animated={true}
         style={[styles.captureButton, { bottom: insets.bottom + 15 }]}
         onPress={async () => {
           const photo = await camera.current?.takePhoto();
+
+          if (photo && photo.path) {
+            const imgW = photo.width;
+            const imgH = photo.height;
+
+            // Inverted because non rotated
+            const scaledW = Math.round(0.85 * imgH);
+            const scaledH = Math.round((175 / winH) * imgW);
+
+            const origPath = `file://${photo.path}`;
+
+            const cropRegion = {
+              x: Math.round(imgW / 2) - Math.round(scaledW / 4),
+              y: Math.round(imgH / 2) - scaledH,
+              width: scaledH,
+              height: scaledW,
+            };
+
+            const croppedImagePath = await RNPhotoManipulator.crop(
+              origPath,
+              cropRegion,
+            );
+
+            const result = await MlkitOcr.detectFromUri(croppedImagePath);
+            console.log(result);
+          }
         }}
       />
     </View>
@@ -68,7 +87,15 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     borderWidth: 3,
     borderColor: 'white',
-  }
+  },
+  cameraOverlay: {
+    position: 'absolute',
+    alignSelf: 'center',
+    width: '85%',
+    height: 175,
+    borderWidth: 2,
+    borderColor: 'rgba(255, 255, 255, 0.75)',
+  },
 });
 
 export default CameraView;
--- src/views/GestionDossier.tsx
+++ src/views/GestionDossier.tsx
@@ -1,73 +1,72 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { FlatList, SafeAreaView, useColorScheme, View, Button, StyleSheet, Text, TouchableOpacity, ActivityIndicator, ScrollView, Dimensions } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { TextInput } from "react-native-paper";
-//import ImagePicker from 'react-native-image-picker';
-var ImagePicker = require('react-native-image-picker');
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  View,
+  Button,
+  StyleSheet,
+  ActivityIndicator,
+  ScrollView,
+  Dimensions,
+} from 'react-native';
+import { TextInput } from 'react-native-paper';
 
-import MlkitOcr, { MlkitOcrResult } from '../../react-native-mlkit-ocr/src/index';
+const ImagePicker = require('react-native-image-picker');
+
+import MlkitOcr from 'react-native-mlkit-ocr';
+import { MlkitOcrResult } from 'react-native-mlkit-ocr/src/index';
 
 import {
   ImagePickerResponse,
   launchImageLibrary,
 } from 'react-native-image-picker';
 
-import { PatientNavigationProps } from '~models/types';
+import {
+  DossierService,
+  CentreDeSanteService,
+  PatientService,
+} from '~services';
 
-import { observer } from 'mobx-react';
+export default function GestionDossier() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [result, setResult] = useState<MlkitOcrResult | undefined>();
 
-import { DossierNavigationProps } from '~models/types';
-import { DossierService, CentreDeSanteService, PatientService } from '~services';
+  const [dataInfo, setDataInfo] = useState<any>(undefined);
 
-export default function GestionDossier(){
-
-  const [loading, setLoading] = React.useState<boolean>(false);
-  const [result, setResult] = React.useState<MlkitOcrResult | undefined>();
-  const [image, setImage] = React.useState<ImagePickerResponse | undefined>();
-
-  const [dataInfo, setDataInfo] = React.useState();
-
-  const [text, setText] = React.useState('');
-
-  async function sendInfo(){
+  async function sendInfo() {
     const centreDeSante = await CentreDeSanteService.get(1);
-    
+
     //On teste si le patient existe.
-    try{
-      let patientExist = await DossierService.getPatientByName(dataInfo[1].value);
-    }
-    catch(e){
-      
-      let date_naissance_complete = (dataInfo[0].value).split("-");
+    try {
+      await DossierService.getPatientByName(dataInfo[1].value);
+    } catch (e) {
+      let date_naissance_complete = dataInfo[0].value.split('-');
 
       //Creation du patient si il n'existe pas
-      let patient = await PatientService.create(
-        {
-          centre_de_sante: centreDeSante,
-          num_dossier: dataInfo[3].value,
-          nom: dataInfo[1].value,
-          assurance_maladie: dataInfo[5].value,
-          assurance_maladie_exp_m: (dataInfo[6].value).slice(2, 4),
-          assurance_maladie_exp_a: (dataInfo[6].value).slice(0, 2),
-          date_naissance: new Date(date_naissance_complete[0], date_naissance_complete[1], date_naissance_complete[2]),
-          sexe: dataInfo[2].value,
-          cellulaire: dataInfo[4].value
-        }
-      )
+      let patient = await PatientService.create({
+        centre_de_sante: centreDeSante,
+        num_dossier: dataInfo[3].value,
+        nom: dataInfo[1].value,
+        assurance_maladie: dataInfo[5].value,
+        assurance_maladie_exp_m: dataInfo[6].value.slice(2, 4),
+        assurance_maladie_exp_a: dataInfo[6].value.slice(0, 2),
+        date_naissance: new Date(
+          date_naissance_complete[0],
+          date_naissance_complete[1],
+          date_naissance_complete[2],
+        ),
+        sexe: dataInfo[2].value,
+        cellulaire: dataInfo[4].value,
+      });
 
       await PatientService.save(patient);
-
     }
-
-
   }
-
 
   function takePhoto(
     setResult: (result: MlkitOcrResult) => void,
     setImage: (result: ImagePickerResponse) => void,
-    setLoading: (value: boolean) => void
-  ){
+    setLoading: (value: boolean) => void,
+  ) {
     var options = {
       storageOptions: {
         skipBackup: true,
@@ -85,145 +84,129 @@ export default function GestionDossier(){
         console.log(source);
       }
     });
-  };
-
+  }
 
   function upload(
     setResult: (result: MlkitOcrResult) => void,
     setImage: (result: ImagePickerResponse) => void,
     setLoading: (value: boolean) => void,
-  ){
-
+  ) {
     launchImageLibrary(
       {
         mediaType: 'photo',
       },
       async (response: ImagePickerResponse) => {
-        if (!response.assets[0].uri) {
-          throw new Error('oh!');
+        if (response && response.assets && response.assets.length > 0) {
+          try {
+            setImage(response);
+            setResult(await MlkitOcr.detectFromUri(response.assets[0].uri));
+
+            if (result) {
+              const nas_complete = result[0].lines[6].text
+                .replace(' ', '')
+                .trim();
+
+              const json = [
+                { value: result[0].lines[0].text, txt: 'date' },
+                { value: result[0].lines[1].text, txt: 'name' },
+                { value: result[1].text, txt: 'sexe' },
+                { value: result[2].text, txt: 'numero patient' },
+                { value: result[3].text, txt: 'telephone' },
+                { value: nas_complete.slice(0, 12), txt: 'nas' },
+                {
+                  value: nas_complete.slice(12, nas_complete.length).trim(),
+                  txt: 'nas exp',
+                },
+              ];
+
+              setDataInfo(json);
+            }
+          } catch (e) {
+            console.error(e);
+          } finally {
+            setLoading(false);
+          }
         }
-        try {
-          setImage(response);
-          const result = await MlkitOcr.detectFromUri(response.assets[0].uri);
-          setResult(result);
-          
-          const nas_complete = (result[0].lines[6].text).replace(" ", "").trim();
-
-          const json = [
-            {"value": result[0].lines[0].text,"txt": "date"}, 
-            {"value":result[0].lines[1].text, "txt":"name"}, 
-            {"value":result[1].text, "txt": "sexe"},
-            {"value":result[2].text, "txt": "numero patient"},
-            {"value":result[3].text, "txt": "telephone"},
-            {"value":nas_complete.slice(0, 12), "txt": "nas"},
-            {"value":nas_complete.slice(12, nas_complete.length).trim(), "txt": "nas exp"}
-          ];
-          
-          setDataInfo(json);
-
-
-        } catch (e) {
-          console.error(e);
-        } finally {
-          setLoading(false);
-        }
-      }
+      },
     );
+  }
 
-  };
-
-
-if (loading) {
+  if (loading) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ActivityIndicator />
+      </SafeAreaView>
+    );
+  }
   return (
     <SafeAreaView style={styles.container}>
-      <ActivityIndicator />
-    </SafeAreaView>
-  );
-}
-return (
-  <SafeAreaView style={styles.container}>
-    {!!result?.length && (
-      <ScrollView
-        contentContainerStyle={{
-          alignItems: 'stretch',
-          padding: 10,
-          height: Dimensions.get('window').height,
-        }}
-        showsVerticalScrollIndicator
-        style={styles.scroll}
-      >
-
-        <Button
-          onPress={() => {
-            sendInfo();
+      {!!result?.length && (
+        <ScrollView
+          contentContainerStyle={{
+            alignItems: 'stretch',
+            padding: 10,
+            height: Dimensions.get('window').height,
           }}
-          title="Send"
-        />
+          showsVerticalScrollIndicator
+          style={styles.scroll}>
+          <Button
+            onPress={() => {
+              sendInfo();
+            }}
+            title="Send"
+          />
 
-        {dataInfo.map((item: any, index: any) => {
-            
-            return(
-              <View
-                style={styles.rowContainer}
-                key={index}
-              >
+          {dataInfo.map((item: any, index: any) => {
+            return (
+              <View style={styles.rowContainer} key={index}>
                 <TextInput
                   label={dataInfo[index].txt}
                   style={styles.textInput}
                   value={dataInfo[index].value}
-                  onChangeText = { (value) => {
-                    dataInfo[index].value = value
+                  onChangeText={value => {
+                    dataInfo[index].value = value;
                     setDataInfo([...dataInfo], dataInfo);
-                  } }
+                  }}
                 />
               </View>
-            )
-
-          })
-        }
-        
-        
-      </ScrollView>
-    )}
-    <View style={styles.buttonsView}>
-      <Button
-        onPress={() => {
-          setLoading(true);
-          upload(setResult, setImage, setLoading);
-        }}
-        title="Upload"
-      />
-      <Button
-        onPress={() => {
-          setLoading(true);
-          takePhoto(setResult, setImage, setLoading);
-        }}
-        title="Take Photo"
-      />
-    </View>
-
-  </SafeAreaView>
-);
-
-
-
+            );
+          })}
+        </ScrollView>
+      )}
+      <View style={styles.buttonsView}>
+        <Button
+          onPress={() => {
+            setLoading(true);
+            upload(setResult, setImage, setLoading);
+          }}
+          title="Upload"
+        />
+        <Button
+          onPress={() => {
+            setLoading(true);
+            takePhoto(setResult, setImage, setLoading);
+          }}
+          title="Take Photo"
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
 
-
 const styles = StyleSheet.create({
-  buttonsView : {
-    flexDirection:"row", 
-    flex:1,
-    alignItems:"flex-start", 
-    justifyContent:"space-between",
-    padding: 20
+  buttonsView: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+    padding: 20,
   },
   button: {
     alignItems: 'center',
     backgroundColor: '#DDDDDD',
     padding: 10,
-    width:300,
-    marginTop:16
+    width: 300,
+    marginTop: 16,
   },
   container: {
     flex: 1,
@@ -236,10 +219,10 @@ const styles = StyleSheet.create({
     borderWidth: 2,
   },
   rowContainer: {
-    flex: 1, 
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
   },
   text: {
     flex: 1,
@@ -247,12 +230,10 @@ const styles = StyleSheet.create({
   },
   textInput: {
     flex: 1,
-    backgroundColor: 'white', 
+    backgroundColor: 'white',
     borderColor: 'black',
     fontSize: 10,
-  }
+  },
 });
-
-
 
 //export default GestionDossier;
